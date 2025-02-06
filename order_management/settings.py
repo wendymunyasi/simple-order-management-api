6c534ecbb62036a4443c72fc8434c90f58cdf70a
Wendy Munyasi <wendymunyasi@gmail.com>
2025-02-06 15:21:58 +0300

    Add postgresql db connection

--- order_management/settings.py
+++ order_management/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "order_management.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ordersapidb",
+        "USER": "burn",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
